ce16a28ec03e29ac159abc99802bfb5ce696a9bb
Jacob Vigeveno <vigevenoj@gmail.com>
2018-11-13 21:07:08 -0800

    Validate that birthday date property is properly formatted

--- src/main/java/com/sharkbaitextraordinaire/birthday/isittheirbirthday/config/BirthdayConfiguration.java
+++ src/main/java/com/sharkbaitextraordinaire/birthday/isittheirbirthday/config/BirthdayConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.MonthDay;
 
 @Component
@@ -13,6 +14,7 @@ public class BirthdayConfiguration {
     @NotEmpty
     private String celebrantName;
     // MonthDay is formatted "--MM-dd", like "--11-14" or "--12-03"
+    @Pattern(regexp="-{2}\\d{2}-\\d{2}")
     private MonthDay birthdayDate;
 
     public String getCelebrantName() {
